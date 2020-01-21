2972f45606f91253dc7b67885eb96c1b6b17d114
duriel666 <59049578+duriel666@users.noreply.github.com>
2020-01-21 22:03:04 +0200

    oamk

---
+++ ohjelmoinnin-perusteet/viikko3/vk3t21/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int var;
+    printf("valitse 1, 2 tai 3.\n");
+    scanf("%d",&var);
+    switch (var) {
+        case 1: printf("valitsit 1\n");
+        break;
+        case 2: printf("valitsit 2\n");
+        break;
+        case 3: printf("valitsit 3\n");
+        break;
+        default: printf("valitsit jotakin muuta\n");
+        break;
+    }
+    return 0;
+}
