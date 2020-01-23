9e5987f63a23cbe617ad27d355d989a557c0d612
duriel666 <59049578+duriel666@users.noreply.github.com>
2020-01-23 18:07:06 +0200

    oamk

---
+++ ohjelmoinnin-perusteet/viikko3/vk3t22/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char var;
+    printf("valitse a, b tai c.\n");
+    scanf("%c",&var);
+    switch (var) {
+        case 'a': printf("painoit aan\n");
+        break;
+        case 'b': printf("painoit been\n");
+        break;
+        case 'c': printf("painoit ceen\n");
+        break;
+        default: printf("vaara merkki\n");
+        break;
+    }
+    return 0;
+}
