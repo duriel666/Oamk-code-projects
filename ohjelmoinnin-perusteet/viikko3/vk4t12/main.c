ad04a0aae458c70aabafdef8501316016d57e74f
duriel666 <59049578+duriel666@users.noreply.github.com>
2020-02-06 07:15:17 +0200

    Update main.c

--- ohjelmoinnin-perusteet/viikko3/vk4t12/main.c
+++ ohjelmoinnin-perusteet/viikko3/vk4t12/main.c
@@ -6,12 +6,12 @@ int main()
     for(int i = 0 ; i < 6 ; i++){
         printf("kokonaislukuja\n");
         scanf("%d", &numbers[i]);
-    }
-    for(int i = 0; i < 6; i++){
-        if(numbers[i] % 2 == 0){
-            parisumma++;
-        }
-    }
+            }
+            for(int i = 0; i < 6; i++){
+                if(numbers[i] % 2 == 0){
+                    parisumma++;
+                }
+            }
     printf("parillisia %d\n", parisumma);
     return 0;
 }
