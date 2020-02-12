1230fd000f5eaae9d9045d0c8510085e97ca835b
duriel666 <59049578+duriel666@users.noreply.github.com>
2020-02-12 15:46:17 +0200

    oamk

--- ohjelmoinnin-perusteet/viikko6/vk6t11/main.c
+++ ohjelmoinnin-perusteet/viikko6/vk6t11/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+void x5(int luku[]);
+void kirjoita(int luku[]);
 
 int main()
 {
-    printf("Hello World!\n");
+    int luku[5];
+    int *ptr = luku;
+    for(int i=0; i<5; i++){
+        printf("kokonaisluku\n");
+        scanf("%d",&luku[i]);
+    }
+    x5(ptr);
     return 0;
+}
+
+void x5(int luku[]){
+    for(int i=0; i<5; i++){
+        luku[i]=luku[i]*5;
+    }
+    kirjoita(luku);
+}
+
+void kirjoita(int luku[]){
+    for(int i=0; i<5; i++){
+        printf("%d\n",luku[i]);
+    }
 }
--- ohjelmoinnin-perusteet/viikko6/vk6t21/main.c
+++ ohjelmoinnin-perusteet/viikko6/vk6t21/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+#define etunimi 20
+#define sukunimi 20
+
 int main()
 {
-    printf("Hello World!\n");
+    struct tiedot{
+        char nimi1[etunimi];
+        char nimi2[sukunimi];
+        int ika;
+    };
+    struct tiedot t;
+    printf("etunimi \n");
+    scanf("%s",t.nimi1);
+    printf("sukunimi \n");
+    scanf("%s",t.nimi2);
+    printf("ika \n");
+    scanf("%d",&t.ika);
+    printf("%s %s %d",t.nimi1,t.nimi2,t.ika);
+
     return 0;
 }
